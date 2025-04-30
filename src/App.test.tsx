c3d6af7801207445faa479bf2bcab6743c7fac0c
Daniel Ma <dma@planningcenter.com>
2025-04-29 18:40:15 -0700

    snapshot after selecting a person

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,37 @@
-import { render } from "@testing-library/react";
-import { expect, it } from "vitest";
+import userEvent from "@testing-library/user-event";
+import { screen, render } from "@testing-library/react";
+import { afterAll, afterEach, beforeAll, expect, it } from "vitest";
 import { Form } from "./App";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
 
-it("fragment", () => {
+const server = setupServer(
+  http.get("/api/movies", () => {
+    return HttpResponse.json([{ id: 1, name: "A cool movie!" }]);
+  }),
+  http.get("/api/movies/1/people", () => {
+    return HttpResponse.json([{ id: 1, name: "Daniel Ma" }]);
+  }),
+);
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+it("initial state", () => {
   const { asFragment } = render(<Form onSubmit={() => void 0} />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("select a movie", async () => {
+  const user = userEvent.setup();
+  const { asFragment } = render(<Form onSubmit={() => void 0} />);
+
+  await screen.findByText("A cool movie!");
+  await user.selectOptions(screen.getByLabelText("Movie:"), ["1"]);
+  await screen.findByText("Daniel Ma");
+  await user.selectOptions(screen.getByLabelText("Actor:"), ["Daniel Ma"]);
+
   expect(asFragment()).toMatchSnapshot();
 });
 

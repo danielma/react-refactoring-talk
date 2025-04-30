351083e6579ef167ed6b88a21103e6742bab539b
Daniel Ma <dma@planningcenter.com>
2025-04-29 17:27:33 -0700

    i have an idea for how i could explain the job

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,9 @@ async function fetchJSON(path: string) {
 }
 
 type Actor = { id: number; name: string };
+type Job = { movieId: number | undefined; actorId: number | undefined };
 
-function Form({ onSubmit }: { onSubmit: (data: object) => unknown }) {
+function Form({ onSubmit }: { onSubmit: (data: Job) => unknown }) {
   const [movieId, setMovieId] = useState<number>();
   const [actorId, setActorId] = useState<number>();
 
@@ -53,18 +54,24 @@ function Form({ onSubmit }: { onSubmit: (data: object) => unknown }) {
   const handleSubmit = useCallback(
     (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      const formData = new FormData(e.currentTarget);
-      onSubmit(Object.fromEntries(formData.entries()));
+
+      onSubmit({ movieId, actorId });
     },
-    [onSubmit],
+    [onSubmit, actorId, movieId],
   );
 
   return (
     <form onSubmit={handleSubmit}>
       <fieldset className="flex flex-col gap-2 m-2 p-2 bg-white border rounded">
         <legend className="font-bold text-lg bg-white px-2 border rounded">
-          Task Editor
+          Choose a job
         </legend>
+        <details>
+          <summary>Instructions</summary>
+          Here, you can pick a job in Hollywood. First, select your movie, then
+          decide if you want to be assigned to a specific actor, or will take
+          any job on set
+        </details>
         <VStack className="">
           <label htmlFor="movieId">Movie:</label>
           <Select
@@ -89,14 +96,20 @@ function Form({ onSubmit }: { onSubmit: (data: object) => unknown }) {
             id="actorId"
             name="actor_id"
             className="flex-grow"
-            onChange={(e) => setActorId(parseInt(e.currentTarget.value, 10))}
+            onChange={(e) =>
+              setActorId(
+                e.currentTarget.value
+                  ? parseInt(e.currentTarget.value, 10)
+                  : undefined,
+              )
+            }
             value={actorId}
           >
             <option selected value="">
               {movieId
                 ? actorsIsLoading
                   ? "Loading..."
-                  : "No actor"
+                  : "Any role is great"
                 : "Select a movie"}
             </option>
             {actors.map((p) => (
@@ -121,14 +134,14 @@ function Select({ className, ...props }: React.HTMLProps<HTMLSelectElement>) {
 }
 
 export default function App() {
-  const [result, setResult] = useState<object>();
+  const [job, setJob] = useState<Job>();
 
   return (
     <VStack className="gap-6">
-      <Form onSubmit={setResult} />
-      {result && (
+      <Form onSubmit={setJob} />
+      {job && (
         <pre className="bg-gray-200 border rounded p-4">
-          {JSON.stringify(result)}
+          {JSON.stringify(job, null, 2)}
         </pre>
       )}
     </VStack>
